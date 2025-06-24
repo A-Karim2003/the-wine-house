b17675b51b2bc3d2ec7a338ede17a0a4c05ee3d9
Abdoulkarim <17a.karim@gmail.com>
2025-06-24 22:51:46 +0100

    - Implement scroll-based section navigation using throttling to prevent rapid firing. - Add left/right arrow button support for manual navigation. - Sync active section with navigation dots. - Allow users to jump to sections via navbar clicks (using data-index).

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*
-Explanation:
+Explanation for throtlling:
 
 1. `isThrottled` is initially false, meaning scroll actions are allowed.
 
@@ -21,7 +21,6 @@ Explanation:
 
 let isThrottled = false;
 let counter = 1;
-
 window.addEventListener("wheel", (e) => {
   if (isThrottled) return;
 
@@ -30,26 +29,61 @@ window.addEventListener("wheel", (e) => {
     isThrottled = false;
   }, 1000);
 
-  if (e.deltaY > 0) {
-    counter++;
-    if (counter > 5) counter = 1;
-  } else if (e.deltaY < 0) {
-    counter--;
-    if (counter < 1) counter = 5;
-  }
+  if (e.deltaY > 0) slideSection("right");
+  else if (e.deltaY < 0) slideSection("left");
 });
 
 // ------------------------------------------------------//
 
 const [leftBtn, rightBtn] = document.querySelectorAll(".arrow-btn");
 const sections = document.querySelectorAll("section");
+const dots = document.querySelectorAll(".dot");
 
 leftBtn.addEventListener("click", () => {
-  sections.forEach((section) => (section.style.zIndex = "0"));
+  slideSection("left");
+});
 
-  counter--;
-  if (counter < 1) counter = 5;
+rightBtn.addEventListener("click", () => {
+  slideSection("right");
+});
+
+function slideSection(leftOrRight) {
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  if (leftOrRight === "left") {
+    counter--;
+    if (counter < 1) counter = 5;
+  }
+
+  if (leftOrRight === "right") {
+    counter++;
+    if (counter > 5) counter = 1;
+  }
 
   const section = document.querySelector(`.section-${counter}`);
-  section.style.zIndex = "10";
+  const dot = document.querySelector(`.dot-${counter}`);
+
+  section.classList.add("active");
+  dot.classList.add("active");
+}
+
+slideSection(); // initial page
+
+// Handle nav section
+
+const navList = document.querySelector(".nav-list");
+
+navList.addEventListener("click", (e) => {
+  const navLink = e.target.closest(".nav-link");
+  if (!navLink) return;
+
+  //? get the links number
+  const index = Number(navLink.dataset.index);
+  counter = index;
+  slideSection();
 });
